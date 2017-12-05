05538fa2de903e8500b799ed54e66550af83d489
Marcus Spiegel <malesch@gmail.com>
2017-12-05 16:53:22 +0100

    Localize view user attributes

--- actions/helper.go
+++ actions/helper.go
@@ -1,13 +1,12 @@
 package actions
 
-import (
-	"github.com/gobuffalo/buffalo"
-)
+import "github.com/gobuffalo/buffalo"
 
 func LocalizeSelect(c buffalo.Context, selectMap map[string]int) map[string]int {
 	locSelectMap := make(map[string]int)
 	for name, id := range selectMap {
-		locSelectMap[T.Translate(c, name)] = id
+		locName, _ := T.Translate(c, name)
+		locSelectMap[locName] = id
 	}
 	return locSelectMap
 }
