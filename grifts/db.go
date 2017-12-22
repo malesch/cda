fcb6dcb96050c892c49e19dc76e3d45b3422cced
Marcus Spiegel <malesch@gmail.com>
2017-12-22 16:12:11 +0100

    Seed test user account

--- grifts/db.go
+++ grifts/db.go
@@ -1,6 +1,7 @@
 package grifts
 
 import (
+	"github.com/cdacontrol/cda/models"
 	"github.com/markbates/grift/grift"
 )
 
@@ -8,8 +9,15 @@ var _ = grift.Namespace("db", func() {
 
 	grift.Desc("seed", "Seeds a database")
 	grift.Add("seed", func(c *grift.Context) error {
-		// Add DB seeding stuff here
-		return nil
+		// Users
+		u := &models.User{
+			Name:                 "cda",
+			Password:             "cda",
+			PasswordConfirmation: "cda",
+			RoleID:               2, // Admin
+			LocaleID:             1, // Deutsch
+		}
+		return models.DB.Create(u)
 	})
 
 })
