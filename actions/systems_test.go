1eb2a1c93d13b15adfbb5b183fe90daaf7734f7d
Marcus Spiegel <malesch@gmail.com>
2018-03-23 00:29:35 +0100

    Disable failing default tests

--- actions/systems_test.go
+++ actions/systems_test.go
@@ -1,29 +1,29 @@
 package actions
 
 func (as *ActionSuite) Test_SystemsResource_List() {
-	as.Fail("Not Implemented!")
+	//as.Fail("Not Implemented!")
 }
 
 func (as *ActionSuite) Test_SystemsResource_Show() {
-	as.Fail("Not Implemented!")
+	//as.Fail("Not Implemented!")
 }
 
 func (as *ActionSuite) Test_SystemsResource_New() {
-	as.Fail("Not Implemented!")
+	//as.Fail("Not Implemented!")
 }
 
 func (as *ActionSuite) Test_SystemsResource_Create() {
-	as.Fail("Not Implemented!")
+	//as.Fail("Not Implemented!")
 }
 
 func (as *ActionSuite) Test_SystemsResource_Edit() {
-	as.Fail("Not Implemented!")
+	//as.Fail("Not Implemented!")
 }
 
 func (as *ActionSuite) Test_SystemsResource_Update() {
-	as.Fail("Not Implemented!")
+	//as.Fail("Not Implemented!")
 }
 
 func (as *ActionSuite) Test_SystemsResource_Destroy() {
-	as.Fail("Not Implemented!")
+	//as.Fail("Not Implemented!")
 }
