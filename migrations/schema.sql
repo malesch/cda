fa8294c4da3cb84a73caadb68d099fa81f559bc2
Marcus Spiegel <malesch@gmail.com>
2018-05-30 10:04:14 +0200

    Upgrade to Buffalo v0.11.1

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -1,21 +1,106 @@
-CREATE TABLE "schema_migration" (
-"version" TEXT NOT NULL
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.4
+-- Dumped by pg_dump version 10.4
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: schema_migration; Type: TABLE; Schema: public; Owner: cda
+--
+
+CREATE TABLE public.schema_migration (
+    version character varying(255) NOT NULL
 );
-CREATE UNIQUE INDEX "version_idx" ON "schema_migration" (version);
-CREATE TABLE "users" (
-"id" TEXT PRIMARY KEY,
-"name" TEXT NOT NULL,
-"password_hash" TEXT NOT NULL,
-"locale_id" integer NOT NULL,
-"role_id" integer NOT NULL,
-"created_at" DATETIME NOT NULL,
-"updated_at" DATETIME NOT NULL
+
+
+ALTER TABLE public.schema_migration OWNER TO cda;
+
+--
+-- Name: systems; Type: TABLE; Schema: public; Owner: cda
+--
+
+CREATE TABLE public.systems (
+    id uuid NOT NULL,
+    ip_address character varying(255) NOT NULL,
+    xbee_gateway_id integer NOT NULL,
+    xbee_channel integer NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
 );
-CREATE TABLE "systems" (
-"id" TEXT PRIMARY KEY,
-"ip_address" TEXT NOT NULL,
-"xbee_gateway_id" integer NOT NULL,
-"xbee_channel" integer NOT NULL,
-"created_at" DATETIME NOT NULL,
-"updated_at" DATETIME NOT NULL
+
+
+ALTER TABLE public.systems OWNER TO cda;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: cda
+--
+
+CREATE TABLE public.users (
+    id uuid NOT NULL,
+    name character varying(255) NOT NULL,
+    password_hash character varying(255) NOT NULL,
+    locale_id integer NOT NULL,
+    role_id integer NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
 );
+
+
+ALTER TABLE public.users OWNER TO cda;
+
+--
+-- Name: systems systems_pkey; Type: CONSTRAINT; Schema: public; Owner: cda
+--
+
+ALTER TABLE ONLY public.systems
+    ADD CONSTRAINT systems_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: cda
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: schema_migration_version_idx; Type: INDEX; Schema: public; Owner: cda
+--
+
+CREATE UNIQUE INDEX schema_migration_version_idx ON public.schema_migration USING btree (version);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
